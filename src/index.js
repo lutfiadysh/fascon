ede6d129bbde965d38a8d5a743d1253329c921ee
lutfiadysh <lutfi020117@gmail.com>
2021-09-21 21:08:32 +0700

    baru

--- src/index.js
+++ src/index.js
@@ -6,25 +6,16 @@ module.exports = async function App(context) {
 };
 
 async function SayHi(context) {
-  await context.sendGenericTemplate([
+  await context.sendButtonTemplate('Fascon Silahkan tekan tombol dibawah ini untuk melihat produk', [
     {
-      title: "Welcome to Peter's Hats",
-      imageUrl: 'https://petersfancybrownhats.com/company_image.png',
-      subtitle: "We've got the right hat for everyone.",
-      defaultAction: {
-        type: 'web_url',
-        url: 'https://peterssendreceiveapp.ngrok.io/view?item=103',
-        messengerExtensions: true,
-        webviewHeightRatio: 'tall',
-        fallbackUrl: 'https://peterssendreceiveapp.ngrok.io/',
-      },
-      buttons: [
-        {
-          type: 'postback',
-          title: 'Start Chatting',
-          payload: 'DEVELOPER_DEFINED_PAYLOAD',
-        },
-      ],
+      type: 'web_url',
+      url: 'https://petersapparel.parseapp.com',
+      title: 'Mulai',
+    },
+    {
+      type: 'postback',
+      title: 'Start Chatting',
+      payload: 'USER_DEFINED_PAYLOAD',
     },
   ]);
 }
